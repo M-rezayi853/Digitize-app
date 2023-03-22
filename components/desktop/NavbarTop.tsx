0535158c6aa81917fa0a2012f2bdfcd4db4dccb2
Mahdi Rezayi <m.rezayi853mh77@gmail.com>
2023-03-22 12:57:54 +0330

    fix container and styles

--- components/desktop/NavbarTop.tsx
+++ components/desktop/NavbarTop.tsx
@@ -5,72 +5,74 @@ interface Props {}
 
 const NavbarTop: FC<Props> = (): JSX.Element => {
   return (
-    <nav className='bg-white py-4 shadow-lg sticky z-10 top-0 flex items-center justify-between px-4'>
-      <ul className='flex items-center gap-x-4 text-slate-800 text-lg'>
-        <li>
-          <Link
-            className='block px-4 py-2 hover:bg-gray-100 rounded-md transition-all duration-500'
-            href={'/'}
-          >
-            LOGO
-          </Link>
-        </li>
-        <li>
-          <Link
-            className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
-            href={'/'}
-          >
-            خانه
-          </Link>
-        </li>
-        <li>
-          <Link
-            className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
-            href={'/'}
-          >
-            تلفن همراه
-          </Link>
-        </li>
-        <li>
-          <Link
-            className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
-            href={'/'}
-          >
-            لپ تاپ
-          </Link>
-        </li>
-        <li>
-          <Link
-            className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
-            href={'/'}
-          >
-            ساعت هوشمند
-          </Link>
-        </li>
-      </ul>
-
-      {/* search bar */}
-      <div className='flex-1 max-w-2xl'>
-        <div className='bg-gray-100 rounded-lg flex items-center'>
-          <span className='px-4'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              className='w-5 h-5'
+    <nav className='hidden md:block bg-white py-4 shadow-lg sticky z-10 top-0'>
+      <div className='2xl:container mx-auto 2xl:max-w-screen-2xl flex items-center justify-between px-4'>
+        <ul className='flex items-center md:gap-x-0 lg:gap-x-4 text-slate-800 md:text-md font-semibold'>
+          <li>
+            <Link
+              className='block px-4 py-2 hover:bg-gray-100 rounded-md transition-all duration-500'
+              href={'/'}
             >
-              <path
-                fillRule='evenodd'
-                d='M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z'
-                clipRule='evenodd'
-              />
-            </svg>
-          </span>
-          <input
-            className='focus:outline-none focus:ring-0 bg-transparent w-full border-none px-0 text-slate-800'
-            type='text'
-            placeholder='جستجوی نام محصول، نام برند، نام مدل و ...'
-          />
+              LOGO
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
+              href={'/'}
+            >
+              خانه
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
+              href={'/'}
+            >
+              تلفن همراه
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
+              href={'/'}
+            >
+              لپ تاپ
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='block px-4 py-2 hover:bg-gray-100 rounded transition-all duration-500'
+              href={'/'}
+            >
+              ساعت هوشمند
+            </Link>
+          </li>
+        </ul>
+
+        {/* search bar */}
+        <div className='flex-1 max-w-xl mr-4'>
+          <div className='bg-gray-100 rounded-lg flex items-center'>
+            <span className='px-4'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                className='w-5 h-5'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z'
+                  clipRule='evenodd'
+                />
+              </svg>
+            </span>
+            <input
+              className='focus:outline-none focus:ring-0 bg-transparent w-full border-none px-0 text-slate-800 md:text-sm lg:text-sm'
+              type='text'
+              placeholder='جستجوی نام محصول، نام برند، نام مدل و ...'
+            />
+          </div>
         </div>
       </div>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,23 +18,23 @@ const HomePage: NextPage<Props> = () => {
       </Head>
 
       <NavbarTop />
-      <main className='bg-gray-100 pt-10 px-4'>
+      <main className='2xl:container mx-auto 2xl:max-w-screen-2xl my-8 px-4'>
         <Navbar />
         <FilterSortbar />
 
         <div className='grid grid-cols-12 md:grid-rows-[50px_minmax(500px,_1fr)] md:gap-8'>
           {/* sidebar */}
-          <div className='hidden md:block col-span-4 lg:col-span-3 row-span-2'>
+          <div className='hidden md:block col-span-4 lg:col-span-3 xl:col-span-2 row-span-2'>
             <Sidebar />
           </div>
 
           {/* sort section */}
-          <div className='hidden md:block col-span-8  lg:col-span-9'>
+          <div className='hidden md:block col-span-8  lg:col-span-9 xl:col-span-10'>
             <Sort />
           </div>
 
           {/* product section */}
-          <div className='col-span-12 md:col-span-8 lg:col-span-9'>
+          <div className='col-span-12 md:col-span-8 lg:col-span-9 xl:col-span-10'>
             <Products />
           </div>
         </div>
