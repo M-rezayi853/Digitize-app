b605f1be92d420609323f83b722039376ede2f69
Mahdi Rezayi <m.rezayi853mh77@gmail.com>
2023-03-23 17:17:23 +0330

    cart summary section

--- components/mobile/CartItems.tsx
+++ components/mobile/CartItems.tsx
@@ -5,7 +5,7 @@ interface Props {}
 
 const CartItems: FC<Props> = (): JSX.Element => {
   return (
-    <div className='flex items-center gap-x-4'>
+    <div className='flex items-start gap-x-4'>
       <div className='flex flex-col gap-y-2 md:gap-y-4 flex-1'>
         <div className='bg-white rounded-lg py-4 px-2 flex items-stretch justify-between shadow-md'>
           <div className='w-20 md:w-24 h-auto my-auto'>
@@ -304,7 +304,49 @@ const CartItems: FC<Props> = (): JSX.Element => {
         </div>
       </div>
 
-      <div className='bg-red-200 hidden md:flex w-1/3'>cart summary</div>
+      <div className='hidden md:flex flex-col w-1/3 max-w-sm'>
+        <div className='bg-white mb-5 px-4 py-5 rounded-lg w-full text-slate-800 font-medium'>
+          <div className='flex items-center justify-between mb-5'>
+            <span className='text-lg'>مجموع قیمت: </span>
+            <span className='text-orange-500'>35,450,000 تومان</span>
+          </div>
+
+          <div className='flex items-center justify-between mb-5'>
+            <span className='font-light text-lg'>کد تخفیف:</span>
+            <div className='flex items-stretch justify-between overflow-hidden rounded-xl bg-gray-200'>
+              <input
+                className='border-0 w-full max-w-[150px] bg-transparent p-2 text-gray-700 outline-none focus:outline-none'
+                type='text'
+                placeholder='AS-DSE'
+                dir='ltr'
+              />
+              <button className='border-0 bg-orange-500 px-4 text-white outline-none'>
+                تایید
+              </button>
+            </div>
+          </div>
+
+          <div className='flex items-center justify-between mb-11'>
+            <span className='text-lg'>تخفیف: </span>
+            <span className='text-orange-500'>450,000 تومان</span>
+          </div>
+
+          <div className='flex items-center justify-between'>
+            <span className='text-lg'>قیمت نهایی: </span>
+            <span className='text-orange-500'>35,000,000 تومان</span>
+          </div>
+        </div>
+
+        <div>
+          <button className='w-full mb-4 text-white bg-orange-500 rounded-md text-lg p-3'>
+            سبد خرید
+          </button>
+
+          <button className='w-full mb-4 text-orange-500 bg-transparent border border-orange-500 p-3 rounded-md text-lg'>
+            انصراف از خرید
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
